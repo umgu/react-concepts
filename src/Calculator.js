e1c0020412ad31ceb0ab05b17ebbfc95dba6c5b6
Umesh Gupta <umesh.gupta@clsz.local>
2023-12-28 10:48:31 +0530

    test

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,6 +5,29 @@ const Calculator = () => {
     const [operand1, setOperand1] = useState(0);
     const [operand2, setOperand2] = useState(0);
 
+    function createMatrixNonOptimized(m, n, defaultValue) {
+        const matrix = [];
+        for (let i = 0; i < m; i++) {
+          const row = [];
+          for (let j = 0; j < n; j++) {
+            row.push(defaultValue);
+          }
+          matrix.push(row);
+        }
+        return matrix;
+      }
+      
+      // Usage example:
+      const rows = 3;
+      const cols = 4;
+      const defaultValue = 0;
+      const myMatrix = createMatrixNonOptimized(rows, cols, defaultValue);
+      
+      // Display the created matrix
+      console.log(myMatrix);
+
+
+
     const setOperand = (val, opNo) => {
         if(opNo === 1) {
             setOperand1(val);
