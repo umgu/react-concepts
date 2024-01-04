0e6522e4c3b54142cc7cc75f8f5adea4ab8cb3c7
Umesh Gupta <umesh.gupta@clsz.local>
2024-01-04 11:24:39 +0530

    imp

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -13,6 +13,30 @@ export default function Counter(props) {
     const [count, setCount] = useState(0);
     // const [disableBtn, setDisableBtn] = useState(false);
 
+    function createMatrix(m, n, defaultValue) {
+
+        
+        const matrix = [];
+
+        for (let i = 0; i < m; i++) {
+          const row = [];
+          for (let j = 0; j < n; j++) {
+            row.push(defaultValue);
+          }
+          matrix.push(row);
+        }
+        return matrix;
+      }
+      
+      // Usage example:
+      const rows = 3;
+      const cols = 4;
+      const defaultValue = 0;
+      const myMatrix = createMatrix(rows, cols, defaultValue);
+      
+      // Display the created matrix
+      console.log(myMatrix);
+
     const incClickHandler = () => {
         setCount(count + 1); // async
         onChange(count + 1);
